8d738771dfe0d46023549018f354a0a1e9105e92
thesystem37backl0 <hhsgjhdghuhdffng@hotmail.com>
2026-09-19 23:23:17 +0300

    Set up basic console application structure
    Initialize a console application with title and colors.

--- C#.cs
+++ C#.cs
@@ -1,1 +1,24 @@
+using System;
 
+namespace RN
+{
+    class Rn1
+    {
+        const string NameProgram = "WindowsXP's Console";
+
+        static void Main()
+        {
+            Console.Title = NameProgram;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+
+            while (true)
+            {
+                Console.Write("TheSl>");
+                string inp = Console.ReadLine();
+                // aqui você coloca a lógica do console (funções, ifs, modularização etc.)
+            }
+        }
+    }
+}
